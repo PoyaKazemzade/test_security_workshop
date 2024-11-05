21cb7485395bcc88a624e579b337e3ebf84b6337
Poya Kazemzade <pouya.pua@gmail.com>
2024-11-05 18:27:03 +0100

    detected and debuged bugs in the regex pattern in Username.java. validate() now accepts numbers and all usernames must contain at least one letter or a number

--- backend/src/main/java/se/yrgo/libraryapp/validators/Username.java
+++ backend/src/main/java/se/yrgo/libraryapp/validators/Username.java
@@ -4,24 +4,24 @@ import java.util.regex.Pattern;
 
 /**
  * This validator checks that the username match our high standard for proper names.
- * 
+ * <p>
  * I.e. no funny characters or whitespace and at least four characters long.
  */
 public final class Username {
-    private static Pattern regex = Pattern.compile("[@._a-zA-Z0-]{4,}");
+    private static Pattern regex = Pattern.compile("^(?=.*[a-zA-Z0-9])[a-zA-Z0-9@._-]{4,}$");
 
-    private Username() {}
+    private Username() {
+    }
 
     /**
      * Validates if the given name is a valid username.
-     * 
+     * <p>
      * A username should be at least four characters long and only
      * contain ASCII letters, numbers and the characters @, ., _ and -.
      * It should not contain any other letters, not even whitespace.
-     * 
+     *
      * @param name the name to check
      * @return true if valid, false if not
-     * 
      */
     public static boolean validate(String name) {
         return regex.matcher(name).matches();
