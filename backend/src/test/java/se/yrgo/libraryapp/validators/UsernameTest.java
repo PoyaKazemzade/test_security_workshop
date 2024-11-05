dd76ae9686f6071ce6b49176ebde79a548e6240c
Poya Kazemzade <pouya.pua@gmail.com>
2024-11-05 18:15:53 +0100

    tested 5 new cases in UsernameTest.java

--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,17 +1,53 @@
 package se.yrgo.libraryapp.validators;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
 
 public class UsernameTest {
     @Test
     void correctUsername() {
-        assertTrue(Username.validate("bosse"));
+        assertThat(Username.validate("bosse")).isTrue();
     }
 
     @Test
     void incorrectUsername() {
-        assertFalse(Username.validate("name with space"));
+        assertThat(Username.validate("name with space")).isFalse();
     }
+
+    @Test
+    void nullInput() {
+        assertThatExceptionOfType(NullPointerException.class)
+                .isThrownBy(() -> Username.validate(null));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"bo", "joe"})
+    void shortUsername(String name) {
+        assertThat(Username.validate(name)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"😀johan", "§", "----"})
+    void invalidCharacters(String username) {
+        assertThat(Username.validate(username)).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Agent_007", "1990Anton"})
+    void digitInputs(String username) {
+        assertThat(Username.validate(username)).isTrue();
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void emptyStringUsername(String username) {
+        assertThat(Username.validate(username)).isFalse();
+    }
+
 }
